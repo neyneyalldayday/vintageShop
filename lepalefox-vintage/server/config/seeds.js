e81351ac5eef897c96f369fa0a916d4326f21805
neyneyalldayday <drfrankenstein123@gmail.com>
2022-06-23 16:53:04 -0500

    server and config folder

---
+++ lepalefox-vintage/server/config/seeds.js
@@ -0,0 +1,69 @@
+const db = require('./connection');
+const {User , Product, Category} = require('../models');
+
+db.once('open', async () => {
+    await Category.deleteMany();
+
+    const categories = await Category.insertMany([
+        {name: 'panties'},
+        {name: 'socks'},
+        {name: 'blouse'},
+        {name: 'pants'},
+        {name: 'shoes'},
+    ]);
+
+    console.log('categories seeded');
+
+
+    await Product.deleteMany();
+
+    const products = await Product.insertMany([
+
+        {
+            name: 'zebra print silk skirt',
+            description:
+              'dope ass zebra skirt!!!',
+            image: 'cookie-tin.jpg',
+            category: categories[0]._id,
+            price: 2.99,
+            quantity: 500
+          },
+          {
+            name: 'turquoise leather pants',
+            description:
+              'cop dem dope leather pants',
+            image: 'canned-coffee.jpg',
+            category: categories[0]._id,
+            price: 1.99,
+            quantity: 500
+          },
+    ]);
+
+    console.log('products seeded')
+
+    await User.deleteMany();
+
+    await User.create({
+     firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id]
+      }
+    ]
+    });
+
+    await User.create({
+        firstName: 'Elijah',
+        lastName: 'Holt',
+        email: 'eholt@testmail.com',
+        password: 'password12345'
+
+    });
+
+    console.log('users seeded');
+
+    process.exit();
+});
