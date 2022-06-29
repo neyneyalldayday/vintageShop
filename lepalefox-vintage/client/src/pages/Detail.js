bcf9600a8f56c2787e30de9511216d12c6053b45
neyneyalldayday <drfrankenstein123@gmail.com>
2022-06-28 21:33:08 -0500

    bug time

--- lepalefox-vintage/client/src/pages/Detail.js
+++ lepalefox-vintage/client/src/pages/Detail.js
@@ -12,7 +12,7 @@ import {
 } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
-import spinner from '../assets/spinner.gif';
+// import spinner from '../assets/spinner.gif';
 
 function Detail() {
   const dispatch = useDispatch();
@@ -109,7 +109,7 @@ function Detail() {
           />
         </div>
       ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+      {loading ? <img src={''} alt="loading" /> : null}
       <Cart />
     </>
   );
