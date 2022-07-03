2a1a4fa313c86934e98b7193b49eb736d550eb5f
neyneyalldayday <drfrankenstein123@gmail.com>
2022-07-03 15:21:38 -0500

    changed some colors

--- lepalefox-vintage/client/src/components/Nav/index.js
+++ lepalefox-vintage/client/src/components/Nav/index.js
@@ -37,9 +37,9 @@ function Nav() {
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag">
-            🛍️
+            🦊 
           </span>
-          -Shop-Shop
+          LePaleFoxVintage
         </Link>
       </h1>
 
