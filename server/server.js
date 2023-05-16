b37b64fe7e74771b95fadb7f35725ca47a8941d9
neyneyalldayday <drfrankenstein123@gmail.com>
2023-05-16 18:38:40 -0500

    work

--- server/server.js
+++ server/server.js
@@ -3,28 +3,17 @@ const { ApolloServer } = require('apollo-server-express');
 const cors = require('cors');
 const path = require('path');
 
-
-const {typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const db = require("./config/connection");
-
-
-
-app.use(cors({
-    origin: 'https://the-vintage-shop.vercel.app',
-    methods: 'POST, GET, OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization'
-  }));
-  
-app.options('*', cors());
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-    debug: true,
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+  debug: true,
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -33,23 +22,32 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+  app.use(express.static('/client/build'));
 }
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-//where is the build dammit
-const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
-    server.applyMiddleware({ app });
+// Enable CORS
+app.use(cors({
+  origin: 'https://the-vintage-shop.vercel.app',
+  methods: 'POST, GET, OPTIONS',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}!`);
-            console.log(`use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
-        })
-    })
+app.options('*', cors());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 };
 
 startApolloServer(typeDefs, resolvers);
