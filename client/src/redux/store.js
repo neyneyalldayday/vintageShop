2715c9d29a5abada3af63a1a1cc7f09e820a2d2b
neyneyalldayday <drfrankenstein123@gmail.com>
2024-11-07 16:14:44 -0600

    86 it all

--- client/src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from '../utils/reducers'
-
-const store = configureStore({ reducer: reducer });
-export default store;
--- server/config/connection.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lepalefox-vintage', {
-   useNewUrlParser: true,
-   useUnifiedTopology: true 
-});
-
-module.exports = mongoose.connection;
